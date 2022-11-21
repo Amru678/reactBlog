ccdacc53b6a89c8f5effce2ec184cf4a0adeda62
Amrutha756 <amrutha_b@persistent.com>
2022-11-21 15:56:58 +0530

    First Commit

---
+++ cooky-blog/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './Navbar.js';
+import Home from './Home';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      <h1>Welcome to my Blog</h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
